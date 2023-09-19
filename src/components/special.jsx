10ffc8b822ce39b4f26ce0affdab1abce7c53e39
Platon61633 <peredreevplaton@gmail.com>
2023-09-19 19:51:41 +0300

    1/.5

--- src/components/special.jsx
+++ src/components/special.jsx
@@ -11,7 +11,7 @@ const Special = () => {
     useEffect(
         ()=> {
             axios.get('http://restoraunt.wuaze.com/index.php?for=special')
-            .then(rsp=> console.log(rsp.data+'ffffff'))
+            .then(rsp=> ArrSpecial(rsp.data+'ffffff'))
             console.log('gg');
         }, []
     )
